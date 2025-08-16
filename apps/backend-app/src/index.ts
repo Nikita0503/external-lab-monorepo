3c4fa6ae8992e12983bb8563b7febd7783f0c5dd
Nikita0503 <nikita0503ua@gmail.com>
2025-08-16 23:09:47 +0300

    chore: adjust common packages

--- apps/backend-app/src/index.ts
+++ apps/backend-app/src/index.ts
@@ -1,10 +1,11 @@
+import { TEST_CONSTANT } from "@external-lab-monorepo/api";
 import express, { Request, Response } from "express";
 
 const app = express();
 const port = 4000;
 
 app.get("/", (req: Request, res: Response) => {
-  res.json({ message: "Hello Turbo Monorepo Backend!" });
+  res.json({ message: TEST_CONSTANT });
 });
 
 app.listen(port, () => {
