bf7fd7a8c4d20704617a8e22a3ad37c071dceb9c
Nikita0503 <nikita0503ua@gmail.com>
2025-08-16 23:42:54 +0300

    chore: adjust React App

---
+++ packages/api/src/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.TEST_CONSTANT = exports.exampleFunctionApi = void 0;
+var exampleFunctionApi = function () { };
+exports.exampleFunctionApi = exampleFunctionApi;
+exports.TEST_CONSTANT = "This is a test constant";
---
+++ packages/store/src/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.exampleFunctionApi = void 0;
+var exampleFunctionApi = function () { };
+exports.exampleFunctionApi = exampleFunctionApi;
