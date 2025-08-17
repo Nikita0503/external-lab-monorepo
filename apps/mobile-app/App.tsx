c1be7d739ac2e5e04a87d0b0936bba8495be4a5c
Nikita0503 <nikita0503ua@gmail.com>
2025-08-17 16:30:58 +0300

    chore: adjust React Native App

---
+++ apps/mobile-app/App.tsx
@@ -0,0 +1,36 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import { TEST_CONSTANT } from '@external-lab-monorepo/api';
+import {
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+function App() {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <View style={styles.container}>
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+      <Text>{TEST_CONSTANT}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default App;
