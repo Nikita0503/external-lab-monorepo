ccfcbc1380940e89cee82d73c77d409ff7777648
Nikita0503 <nikita0503ua@gmail.com>
2025-08-19 15:58:14 +0300

    chore: create DB for backend app

--- packages/api/src/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TEST_CONSTANT = exports.exampleFunctionApi = void 0;
-var exampleFunctionApi = function () { };
-exports.exampleFunctionApi = exampleFunctionApi;
-exports.TEST_CONSTANT = "This is a test constant";
--- packages/store/src/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.exampleFunctionApi = void 0;
-var exampleFunctionApi = function () { };
-exports.exampleFunctionApi = exampleFunctionApi;
