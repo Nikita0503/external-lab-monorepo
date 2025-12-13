e36db98507314d5128264516e01ae368f0c90bbd
Nikita0503 <nikita0503ua@gmail.com>
2025-12-13 16:29:48 +0200

    [packages/store] feat: create auth related store

--- packages/store/src/actions/authActions.ts
+++ packages/store/src/actions/authActions.ts
@@ -1,7 +1,10 @@
-import { createAction } from "@reduxjs/toolkit";
+import { loginApi, registrationApi } from "@external-lab-monorepo/api";
+import { createAction, createAsyncThunk } from "@reduxjs/toolkit";
 import {
   ISetAccessTokenAction,
   ISetLoadingAction,
+  ISignInAsyncAction,
+  ISignUpAsyncAction,
 } from "../interfaces/actions/authActions";
 
 export const setAccessTokenAction = createAction<ISetAccessTokenAction>(
@@ -10,4 +13,72 @@ export const setAccessTokenAction = createAction<ISetAccessTokenAction>(
 
 export const setLoadingAction = createAction<ISetLoadingAction>(
   "auth/setLoadingAction"
+);
+
+export const signInAsyncAction = createAsyncThunk<void, ISignInAsyncAction>(
+  "auth/signInAsyncAction",
+  async (
+    { email, password, onSuccess, onError }: ISignInAsyncAction,
+    { dispatch }
+  ) => {
+    try {
+      dispatch(setLoadingAction({ loading: true }));
+      const res = await loginApi(email.toLowerCase(), password);
+      if (res.data.access_token) {
+        dispatch(setAccessTokenAction({ accessToken: res.data.access_token }));
+      }
+      if (onSuccess) {
+        onSuccess();
+      }
+    } catch (e: any) {
+      console.log("🔴 authActions::signInAsyncAction error:", e);
+      if (onError) {
+        onError(e);
+      }
+    } finally {
+      dispatch(setLoadingAction({ loading: false }));
+    }
+  }
+);
+
+export const signUpAsyncAction = createAsyncThunk<void, ISignUpAsyncAction>(
+  "auth/signUpAsyncAction",
+  async (
+    {
+      email,
+      name,
+      password,
+      repeatPassword,
+      avatar,
+      onSuccess,
+      onError,
+    }: ISignUpAsyncAction,
+    { dispatch }
+  ) => {
+    try {
+      dispatch(setLoadingAction({ loading: true }));
+      if (password !== repeatPassword) {
+        return;
+      }
+      const res = await registrationApi(
+        email.toLowerCase(),
+        name,
+        password,
+        avatar
+      );
+      if (res.data.access_token) {
+        dispatch(setAccessTokenAction({ accessToken: res.data.access_token }));
+      }
+      if (onSuccess) {
+        onSuccess();
+      }
+    } catch (e: any) {
+      console.log("🔴 authActions::signUpAsyncAction error:", e);
+      if (onError) {
+        onError(e);
+      }
+    } finally {
+      dispatch(setLoadingAction({ loading: false }));
+    }
+  }
 );
