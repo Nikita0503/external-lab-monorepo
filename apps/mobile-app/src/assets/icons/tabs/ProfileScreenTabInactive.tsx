95de5d86fdd6bd7a796879a7401a036f2ee5de43
Nikita0503 <nikita0503ua@gmail.com>
2026-01-31 15:21:39 +0200

    [apps/mobile-app] fix: fix tab icons size

--- apps/mobile-app/src/assets/icons/tabs/ProfileScreenTabInactive.tsx
+++ apps/mobile-app/src/assets/icons/tabs/ProfileScreenTabInactive.tsx
@@ -3,8 +3,8 @@ import Svg, { Path } from 'react-native-svg';
 
 const ProfileScreenTabInactive = (props: any) => (
   <Svg
-    width={45}
-    height={45}
+    width={30}
+    height={30}
     viewBox="0 0 45 45"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
