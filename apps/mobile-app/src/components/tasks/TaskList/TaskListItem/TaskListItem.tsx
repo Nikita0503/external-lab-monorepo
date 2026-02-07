29724a4acc03f5050bbfed2836674be492e9a92b
Nikita0503 <nikita0503ua@gmail.com>
2026-02-07 18:48:32 +0200

    [apps/mobile-app] feat: display task priority on TaskListItem depends on sprint

--- apps/mobile-app/src/components/tasks/TaskList/TaskListItem/TaskListItem.tsx
+++ apps/mobile-app/src/components/tasks/TaskList/TaskListItem/TaskListItem.tsx
@@ -1,9 +1,11 @@
+import { SPRINTS } from '@external-lab-monorepo/constants';
 import React from 'react';
 import { Pressable, Text, TouchableOpacity, View } from 'react-native';
 import TaskActiveSvgImage from '../../../../assets/icons/TaskActiveSvgImage';
 import TaskCompletedSvgImage from '../../../../assets/icons/TaskCompletedSvgImage';
 import TaskDeleteSvgImage from '../../../../assets/icons/TaskDeleteSvgImage';
 import TaskEditSvgImage from '../../../../assets/icons/TaskEditSvgImage';
+import { useDevMenu } from '../../../../contexts/DevMenuContext';
 import styles from './TaskListItem.styles';
 import { IProps } from './TaskListItem.types';
 import TaskListItemPriority from './TaskListItemPriority';
@@ -15,6 +17,8 @@ const TaskListItem = ({
   onDeleteTaskPress,
   onSwitchDonePress,
 }: IProps) => {
+  const { sprint } = useDevMenu();
+
   const handleSwitchDonePress = () => {
     onSwitchDonePress(task);
   };
@@ -39,7 +43,9 @@ const TaskListItem = ({
       >
         {task.done ? <TaskCompletedSvgImage /> : <TaskActiveSvgImage />}
       </Pressable>
-      {task.priority && <TaskListItemPriority priority={task.priority} />}
+      {sprint === SPRINTS.SPRINT_4 && task.priority && (
+        <TaskListItemPriority priority={task.priority} />
+      )}
       <View style={styles.content}>
         <Text
           numberOfLines={1}
