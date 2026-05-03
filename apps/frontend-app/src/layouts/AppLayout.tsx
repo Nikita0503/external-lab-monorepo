028b45c69a8b8b353863b96af4992d85dc315c7b
Nikita0503 <nikita0503ua@gmail.com>
2026-05-03 11:49:52 +0300

    [apps/frontend-app] feat: navigation improvements

--- apps/frontend-app/src/layouts/AppLayout.tsx
+++ apps/frontend-app/src/layouts/AppLayout.tsx
@@ -1,8 +1,18 @@
+import { SPRINTS } from "@external-lab-monorepo/constants";
 import { NavLink, Outlet } from "react-router-dom";
+import { useDevMenu } from "../contexts/DevMenuContext";
+import StubPage from "../pages/stubs/StubPage";
 import { ROUTES } from "../router/routes";
 import "./AppLayout.css";
 
 const AppLayout = () => {
+  const { sprint } = useDevMenu();
+
+  // Sprint 1: stub instead of entire app
+  if (sprint === SPRINTS.SPRINT_1) {
+    return <StubPage />;
+  }
+
   return (
     <div className="app-layout">
       <nav className="app-nav">
@@ -12,9 +22,12 @@ const AppLayout = () => {
         <NavLink to={ROUTES.PROFILE} className="app-nav-link">
           Profile
         </NavLink>
-        <NavLink to={ROUTES.COMMON_TASKS} className="app-nav-link">
-          Common Tasks
-        </NavLink>
+        {/* Sprint 1 & 2: no Common Tasks tab */}
+        {sprint !== SPRINTS.SPRINT_2 && (
+          <NavLink to={ROUTES.COMMON_TASKS} className="app-nav-link">
+            Common Tasks
+          </NavLink>
+        )}
       </nav>
       <main className="app-main">
         <Outlet />
